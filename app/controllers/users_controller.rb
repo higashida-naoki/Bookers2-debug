5af0340b8d170e92829683816c7ff4b5b73f8aa2
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-02 04:37:05 +0000

    [finish]Bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
   end
