9193f5da5ef962cba0a9cb0900fe57c83fe3f9a0
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-05 15:04:04 +0000

    [finish]bookers2-検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
+    get '/search', to: 'searches#search'
   end
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get '/search', to: 'searches#search'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
